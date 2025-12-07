d273b2cece44a36f57a33abf90f0a57115fd7631
Davi-Oliveira214 <davi.santosoliveira214@gmail.com>
2025-12-07 16:41:50 -0300

    Animando o burguer e retirando códigos do js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
-const modelo = document.getElementById("modelo");
-const abrirBtn = document.getElementById("abrirModelo");
-const fecharBtn = document.getElementById("fecharModelo");
+const burguer = document.getElementById("burguer");
+const menu_navegacao = document.getElementById("menu-navegacao");
 
-abrirBtn.onclick = () => (modelo.style.display = "flex");
-fecharBtn.onclick = () => (modelo.style.display = "none");
+burguer.addEventListener("click", () => {
+  burguer.classList.toggle("active");
+  menu_navegacao.style.display = "flex";
+  menu_navegacao.classList.remove("fechar-menu");
+  menu_navegacao.classList.add("abrir-menu");
+});
 
-window.onclick = (e) => {
-  if (e.target === modelo) modelo.style.display = "none";
-};
+menu_navegacao.addEventListener("click", () => {
+  burguer.classList.remove("active");
+  menu_navegacao.classList.remove("abrir-menu");
+  menu_navegacao.classList.add("fechar-menu");
 
-function menu(resposta) {
-  const menu_navegacao = document.getElementById("menu-navegacao");
-  const menu = document.getElementById("teste");
+  setTimeout(() => {
+    menu_navegacao.style.display = "none";
+  }, 300);
+});
 
-  if (resposta === "abrir") {
-    menu_navegacao.style.display = "flex";
-    menu_navegacao.classList.remove("fechar-menu");
-    menu_navegacao.classList.add("abrir-menu");
-  } else if (resposta === "fechar") {
-    menu_navegacao.classList.remove("abrir-menu");
-    menu_navegacao.classList.add("fechar-menu");
+// const modelo = document.getElementById("modelo");
+// const abrirBtn = document.getElementById("abrirModelo");
+// const fecharBtn = document.getElementById("fecharModelo");
 
-    setTimeout(() => {
-      menu_navegacao.style.display = "none";
-    }, 300);
-  }
-}
+// abrirBtn.onclick = () => (modelo.style.display = "flex");
+// fecharBtn.onclick = () => (modelo.style.display = "none");
+
+// window.onclick = (e) => {
+//   if (e.target === modelo) modelo.style.display = "none";
+// };
