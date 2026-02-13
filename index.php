eb69944b6f845e46e9dcd57b0c5d16ab6d7a661e
Davi-Oliveira214 <davi.santosoliveira214@gmail.com>
2026-02-13 09:23:47 -0300

    Alteração do card e conectando serviços ao banco de dados

--- index.php
+++ index.php
@@ -197,18 +197,23 @@ include("./includes/cabecalho.php");
             $horaInicio = date('H:i', strtotime($servico['horario_inicio']));
             $horaFim = date('H:i', strtotime($servico['horario_fim']));
 
-            echo "<article class=card-servicos data-categoria=$servico[id_categoria]>";
-            echo "<img src=$servico[imagem] alt=Eletricista>";
-            echo "<div class=card-conteudo>";
-            echo "<div class=card-sobre>";
-            echo "<h3>$servico[nome]</h3>";
+            echo "<div class='card card-servico'>";
+            echo "<img src='$servico[imagem]' alt=''>";
+            echo "<div>";
+            echo "<div class='info-card'>";
+            echo "<h2 class='titulo-card'>$servico[nome]</h2>";
             echo "<p>$servico[descricao]</p>";
             echo "</div>";
-            echo "<data value=class=data-servico>Disponível: $horaInicio</data>";
-            echo "<data value=class=data-servico>Disponível: $horaFim</data>";
-            echo "<button class='btn botao-ver-mais' onclick=modelo(abrir)>Ver detalhes</button>";
+            echo "<div class='cronograma'>";
+            echo "<p>Das <time datetime='$horaInicio'>$horaInicio</time>
+                    Até <time datetime='$horaFim'>$horaFim</time></p>";
+            echo "<p>Data limite: <time datetime='$servico[data_limite]'>$servico[data_limite]</time></p>";
             echo "</div>";
-            echo "</article>";
+            echo "<div class='botoes-card'>";
+            echo "<a href='' class='btn' >Agendar serviço</a>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
         } ?>
     </section>
 
--- servicos.php
+++ servicos.php
@@ -6,145 +6,75 @@ include "./includes/cabecalho.php";
 <main class="principal">
     <section class="avisos-eventos">
         <div class="reservados quadro">
-            <h2>Contratados</h2>
+            <h1>Contratados</h1>
             <div class="box">
                 <?php
-                // $sql = mysqli_query($con, )
+                $servicos = mysqli_query($con, "SELECT * FROM contratados WHERE id_cliente = 1");
+
+                if ($servicos->num_rows) {
+                    while ($servico - mysqli_fetch_assoc($servicos)) {
+                        $horario = date('H:i', strtotime($servico['horario']));
+
+                        echo "<div class='card card-servico'>";
+                        echo "<img src='$servico[imagem]' alt=''>";
+                        echo "<div>";
+                        echo "<div class='info-card'>";
+                        echo "<h2 class='titulo-card'>$servico[nome]</h2>";
+                        echo "<p>$servico[descricao]</p>";
+                        echo "</div>";
+                        echo "<div class='cronograma'>";
+                        echo "<p>Das <time datetime='$horario'>$horario</time>";
+                        echo "<p>Data limite: <time datetime='2026-07-01'>$servico[data]</time></p>";
+                        echo "</div>";
+                        echo "<div class='botoes-card'>";
+                        echo "<a href='' class='btn'>Remarcar</a>";
+                        echo "<a href='' class='btn'>Cancelar</a>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                    }
+                } else {
+                    echo "<h2 class=aviso>Nenhum serviço contratado</h2>";
+                }
                 ?>
-                <article class="card-servicos">
-                    <img src="./img/baba.jpg" alt=Eletricista>
-                    <div class="card-conteudo">
-                        <div class="card-sobre">
-                            <h3>Baba</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam vel adipisci, fuga
-                                eius unde libero quos nisi aut, sunt nihil, dolore voluptatem provident iusto ipsam
-                                aperiam atque impedit dignissimos eligendi!</p>
-                            <div class="horarios">
-                                <data class="data-servico">Marccado: 01/08/2026</data>
-                                <p>Horário: 14:00</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-btn">
-                        <a href="" class="btn remarcar">Remarcar</a>
-                        <a href="" class="btn cancelar">Cancelar</a>
-                    </div>
-                </article>
             </div>
         </div>
         <div class="avisos quadro">
-            <h2>Quadro de Avisos</h2>
+            <h1>Quadro de Avisos</h1>
             <div class="box">
-                <article class="card-avisos">
-                    <div class="titulo-avisos">
-                        <img src="./img/icone.png" alt="">
-                        <div>
-                            <h2>Titulo de teste</h2>
-                            <p>Data </p>
-                        </div>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nam facere repudiandae dolor
-                        dolore, laudantium quo error molestiae totam pariatur recusandae quidem illum natus soluta
-                        perspiciatis repellat? Quod, at impedit.</p>
-                    <img src="./img/a-mostra.jpg" alt="" class="img-aviso">
-                </article>
-                <article class="card-avisos">
-                    <div class="titulo-avisos">
-                        <img src="./img/icone.png" alt="">
-                        <div>
-                            <h2>Titulo de teste</h2>
-                            <p>Data </p>
-                        </div>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea nam facere repudiandae dolor
-                        dolore, laudantium quo error molestiae totam pariatur recusandae quidem illum natus soluta
-                        perspiciatis repellat? Quod, at impedit.</p>
-                    <img src="./img/a-mostra.jpg" alt="" class="img-aviso">
-                </article>
+
             </div>
         </div>
     </section>
 
     <section class="servicos">
-        <h2>Serviços</h2>
+        <h1>Serviços</h1>
         <section class="sessao-servicos">
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
-            <article class="card-servicos">
-                <img src="./img/eletricista.jpg" alt="Eletricista">
-                <div class="card-conteudo">
-                    <div class="card-sobre">
-                        <h3>Eletricista</h3>
-                        <p>Instalações residenciais, manutenção de quadros e reparos emergenciais.</p>
-                    </div>
-                    <data value="" class="data-servico">Disponível: 15/12 a 20/12</data>
-                    <button class="btn botao-ver-mais" onclick="modelo('abrir')">Ver detalhes</button>
-                </div>
-            </article>
+            <?php
+            $servicos = mysqli_query($con, "SELECT * FROM servicos");
+
+            while ($servico = mysqli_fetch_assoc($servicos)) {
+                $horaInicio = date('H:i', strtotime($servico['horario_inicio']));
+                $horaFim = date('H:i', strtotime($servico['horario_fim']));
+
+                echo "<div class='card card-servico'>";
+                echo "<img src='$servico[imagem]' alt=''>";
+                echo "<div>";
+                echo "<div class='info-card'>";
+                echo "<h2 class='titulo-card'>$servico[nome]</h2>";
+                echo "<p>$servico[descricao]</p>";
+                echo "</div>";
+                echo "<div class='cronograma'>";
+                echo "<p>Das <time datetime='$horaInicio'>$horaInicio</time>
+                    Até <time datetime='$horaFim'>$horaFim</time></p>";
+                echo "<p>Data limite: <time datetime='$servico[data_limite]'>$servico[data_limite]</time></p>";
+                echo "</div>";
+                echo "<div class='botoes-card'>";
+                echo "<a href='' class='btn' >Agendar serviço</a>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+            } ?>
         </section>
     </section>
 </main>
