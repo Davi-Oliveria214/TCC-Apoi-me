0111b6475df98e52ab0d7e6b11a0ca18283d9913
Davi-Oliveira214 <davi.santosoliveira214@gmail.com>
2026-02-14 09:30:48 -0300

    Mudando arquivo app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const img = document.getElementById('banner')
+const topo = document.getElementById('topo')
+const inicial = document.getElementById('inicial')
+
+function ajustarTamanho() {
+  let calc = img.offsetHeight - topo.offsetHeight
+  inicial.style.minHeight = `${calc}px`
+}
+
+window.addEventListener('resize', ajustarTamanho)
+window.addEventListener('load', ajustarTamanho)
 
 // Filtra os serviços por categoria
 function filtrar(categoria) {
@@ -11,64 +22,3 @@ function filtrar(categoria) {
     }
   });
 }
-
-// Pega os elementos do menu
-const burguer = document.getElementById("burguer");
-const menu = document.getElementById("menu-navegacao");
-
-// Abre o menu ao clicar no burguer
-burguer.addEventListener("click", () => {
-  burguer.classList.toggle("active");
-  menu.style.display = "flex";
-  menu.classList.remove("fechar-menu");
-  menu.classList.add("abrir-menu");
-});
-
-// Fecha o menu ao clicar em algum link
-menu.addEventListener("click", () => {
-  burguer.classList.remove("active");
-  menu.classList.remove("abrir-menu");
-  menu.classList.add("fechar-menu");
-
-  setTimeout(() => {
-    menu.style.display = "none";
-  }, 300);
-});
-
-// Abre e fecha o modelo
-function modelo(resp) {
-  const modelo = document.getElementById("modelo");
-  if (resp === "abrir") {
-    modelo.style.display = "flex";
-  } else {
-    modelo.style.display = "none";
-  }
-
-  const modeloNoticia = document.getElementById("modeloNoticia");
-  if (resp === "abrirNoticia") {
-    modeloNoticia.style.display = "flex";
-  } else {
-    modeloNoticia.style.display = "none";
-  }
-}
-
-// Pega as informações do perfil
-const infoPerfil = document.getElementById("btn-info-perfil");
-const info = document.getElementById("info-perfil");
-
-const teste = document.body;
-
-// Abre e fecha as informações do perfil
-var infoAberta = false;
-infoPerfil.addEventListener("click", () => {
-  if (infoAberta == false) {
-    info.classList.remove("fechar-info-perfil");
-    info.classList.add("abrir-info-perfil");
-    infoPerfil.textContent = "Fechar informações do perfil";
-  } else {
-    info.classList.remove("abrir-info-perfil");
-    info.classList.add("fechar-info-perfil");
-    infoPerfil.textContent = "Ver informações do perfil";
-  }
-  infoAberta = !infoAberta;
-});
