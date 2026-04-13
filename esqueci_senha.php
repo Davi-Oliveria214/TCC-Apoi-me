ca09d8f87596ff31d49fd159fa4a07e866031c09
unknown <davi.santosoliveira214@gmail.com>
2026-04-13 17:31:30 -0300

    Correção nas autenticações

--- esqueci_senha.php
+++ esqueci_senha.php
@@ -7,7 +7,7 @@ include('./util/avisos.php');
     <main class="autenticar">
         <div class="recover-container">
             <div class="recover-card">
-                <h1 class="recover-title">Recupere sua Senha</h1>
+                <h1 class="recover-title">Enviar código</h1>
                 <p class="recover-subtitle">Insira seu email. Um código de redefinição será enviado.</p>
                 <form action="./controls/enviar_recuperacao.act.php" method="post" class="recover-form">
                     <input type="email" name="email" placeholder="Digite seu email" class="recover-input" required>
