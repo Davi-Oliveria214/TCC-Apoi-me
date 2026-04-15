6e28cdbfb37ba6caa1778141db3688047aac018b
unknown <davi.santosoliveira214@gmail.com>
2026-04-15 16:38:06 -0300

    Verificando verificação de email

--- controls/enviar_recuperacao.act.php
+++ controls/enviar_recuperacao.act.php
@@ -6,7 +6,7 @@ require_once(__DIR__ . '/../util/enviar_email.php');
 $email = trim($_POST['email']);
 $codigo = rand(100000, 999999);
 
-$usuario = request("usuarios?email=eq." . urlencode($email) . "&select=id,nome", "GET");
+$usuario = request("usuarios?email=eq." . urlencode($email) . "&select=id,nome,email_verificado", "GET");
 $nome = $usuario[0]['nome'];
 
 if (empty($usuario) || isset($usuario['error'])) {
@@ -28,6 +28,12 @@ if (isset($res_update['error'])) {
     exit;
 } else {
     $_SESSION['email_verificar'] = $email;
+    if (!$usuario[0]['email_verificado']) {
+        enviarEmail($email, $nome, $codigo, 'cadastro');
+        header("Location: ../aviso_codigo.php");
+        exit;
+    }
+
     enviarEmail($email, $nome, $codigo, 'recuperar');
     header("Location: ../aviso_codigo.php");
     exit;
