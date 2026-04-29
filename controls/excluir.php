569170aa30bfee06094dbe2826058d38a2d92d32
davil <davi.santosoliveira214@gmail.com>
2026-04-28 21:08:03 -0300

    Adicionando modo de remover imagem do banco

--- controls/excluir.php
+++ controls/excluir.php
@@ -6,6 +6,7 @@ require_once(__DIR__ . '/../conexao.php');
 
 $id = $_SESSION['id'];
 $id_servico = $_POST['id_servico'];
+$bucket = $_ENV['BALDE'];
 
 $verificar = request("servicos?id_prestador=eq.{$id}&id=eq.{$id_servico}", "GET");
 
@@ -15,12 +16,35 @@ if (empty($verificar) || isset($verificar['error'])) {
     exit;
 }
 
+$urlImagemSalva = $verificar[0]['imagem'];
+
+if (!empty($urlImagemSalva)) {
+    $nomeFinal = basename(parse_url($urlImagemSalva, PHP_URL_PATH));
+
+    $urlStorage = trim($_ENV['SUPABASE_URL']) . "/storage/v1/object/$bucket/$nomeFinal";
+
+    $ch = curl_init($urlStorage);
+
+    curl_setopt_array($ch, [
+        CURLOPT_CUSTOMREQUEST => "DELETE",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_HTTPHEADER => [
+            "Authorization: Bearer " . trim($_ENV['BALDE_KEY'])
+        ]
+    ]);
+
+    $responseStorage = curl_exec($ch);
+    $statusStorage = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+}
+
 $del = request("servicos?id_prestador=eq.{$id}&id=eq.{$id_servico}", "DELETE");
 
 if (isset($del['error'])) {
-    $_SESSION["mensagem"] = "Não foi possível excluir o serviço.";
+    $_SESSION["mensagem"] = "Não foi possível excluir o serviço do banco.";
 } else {
-    $_SESSION["mensagem"] = "Serviço excluído com sucesso!";
+    $_SESSION["mensagem"] = "Serviço e imagem excluídos com sucesso!";
 }
 
 header("Location: ../anunciar.php");
